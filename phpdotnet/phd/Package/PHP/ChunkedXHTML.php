c09ce4fac1bea7d6c0790148b2b214fe83ccd6d8
Elf Sundae <elf.sundae@gmail.com>
2025-11-13 01:08:41 +0800

    Add shortcut header

--- phpdotnet/phd/Package/PHP/ChunkedXHTML.php
+++ phpdotnet/phd/Package/PHP/ChunkedXHTML.php
@@ -147,6 +147,11 @@ class Package_PHP_ChunkedXHTML extends Package_PHP_Web {
               </style>
               <title>$title</title>
               {$cssLinks}
+              <link rel="icon" type="image/svg+xml" sizes="any" href="favicon.svg?v=2">
+              <link rel="icon" type="image/png" sizes="196x196" href="favicon-196x196.png?v=2">
+              <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png?v=2">
+              <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png?v=2">
+              <link rel="shortcut icon" href="favicon.ico?v=2">
              </head>
              <body class="docs">
         HTML;
